698f5596a6c370995eb1da2ed11f9283e2f72f61
C Sharper <c-sharper-@outlook.com>
2018-04-29 10:40:46 -0400

    Agregar archivos de proyecto.

---
+++ UnitTestTutorial/CalculatorTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTutorial
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_FourAndSix_ReturnsTen()
+        {
+            //Arrange
+            decimal a = 4;
+            decimal b = 6;
+            //Act
+            var result = Calculator.Add(a, b);
+            //Assert
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Substract_EqualValues_ReturnsZero()
+        {
+            decimal a = 8;
+            decimal b = a;
+
+            var result = Calculator.Substract(a, b);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Multiply_AnyValueAndZero_ReturnsZero()
+        {
+            decimal a = 56;
+            decimal b = 0;
+
+            var result = Calculator.Multiply(a, b);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Multiply_ThreeAndFour_ReturnsTwelve()
+        {
+            decimal a = 3;
+            decimal b = 4;
+
+            var result = Calculator.Multiply(a, b);
+
+            Assert.AreEqual(12, result);
+        }
+
+        [TestMethod]
+        public void Divide_ByZero_ThrowsException()
+        {
+            decimal a = 34;
+            decimal b = 0;
+            var mensaje = string.Empty;
+            try
+            {
+                Calculator.Divide(a, b);
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+
+            Assert.AreEqual("No se puede dividir entre cero.", mensaje);
+        }
+
+        [TestMethod]
+        public void Divide_TwentyAndTwo_ReturnsTen()
+        {
+            decimal a = 20;
+            decimal b = 2;
+
+            var result = Calculator.Divide(a, b);
+
+            Assert.AreEqual(10, result);
+        }
+    }
+}
---
+++ UnitTestTutorial/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTutorial")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTutorial")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("556f4526-9b70-4ad9-ac51-d34da2870186")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
